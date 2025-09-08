f0e5052f930bd28838ef6002778346de94a0ba35
Mario Guerra <maguerramorales@gmail.com>
2025-09-08 16:06:59 -0600

    feat: factorial tests

--- tests/test_basicmath.py
+++ tests/test_basicmath.py
@@ -1,5 +1,5 @@
 import pytest
-from basicmathlib.basicmath import square
+from basicmathlib.basicmath import square, factorial
 
 # ---------------- square ----------------
 def test_square_happy_paths():
@@ -11,3 +11,14 @@ def test_square_errors():
         square("3")  # not numeric
     with pytest.raises(ValueError):
         square(float("inf"))
+
+# ---------------- factorial ----------------
+def test_factorial_happy_paths():
+    assert factorial(0) == 1
+    assert factorial(5) == 120
+
+def test_factorial_errors():
+    with pytest.raises(ValueError):
+        factorial(-1)
+    with pytest.raises(TypeError):
+        factorial(3.5)  # must be integer
